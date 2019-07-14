afda0660a33416a2eac658deda965b7a20467afc
ajayraj <ajayraj98@gmail.com>
2019-07-13 23:44:02 -0700

    gh-pages deploy

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,8 +6,8 @@ const Footer = () => (
         <div className="bottom">
             <ul id='footer-list'>
                 <li><span><img className='logo' id="reactLogo" src="assets/images/react-logo.png"/></span> Clicky Game!</li>
-                <li>© 2018 Evan Tucker</li>
-                <li><a href="https://github.com/evanejtucker/clicky-game" target="blank"><span><img className='logo' src="assets/images/github.png"/></span> Github</a></li>
+                <li></li>
+                <li><a href="https://github.com/ajayrajyadav/clicky-game.git" target="blank"><span><img className='logo' src="assets/images/github.png"/></span> Github</a></li>
             </ul>
         </div>
     </footer>
