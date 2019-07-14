dc6d42a81348536a9d8df18013246ccd306ed249
ajayraj <ajayraj98@gmail.com>
2019-07-13 23:25:51 -0700

    Updates

---
+++ precache-manifest.c8c16ac39bdcef37889c86bdae3ec2d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fe97dd155fe4372dd2bb3215c67865c",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "c7c1a21d6793befb6781",
+    "url": "/clicky-game/static/css/main.0f26a32e.chunk.css"
+  },
+  {
+    "revision": "0c7e84a38a9644c12e28",
+    "url": "/clicky-game/static/js/2.564cde6c.chunk.js"
+  },
+  {
+    "revision": "c7c1a21d6793befb6781",
+    "url": "/clicky-game/static/js/main.f6453dcd.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
